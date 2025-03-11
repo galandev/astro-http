a2f1e241abd2482051b1ce5f2890cb6f4ab17d90
Roberto Galán <robertogalan@MacBook-Pro-de-Roberto.local>
2025-03-11 21:32:20 +0100

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: "static",
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
 });
